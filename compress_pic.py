4bb224e9ba62cbbb0c769a58e4b232f8e7530559
Yangxulight <yangxulight@outlook.com>
2018-05-03 13:19:57 +0800

    Add compress picture

--- compress_pic.py
+++ compress_pic.py
@@ -2,13 +2,13 @@ from PIL import Image
 from os import path
 import sys
 
-# usage: python picture1.jepg 180 240
+# usage: python picture1.jepg 8
 if __name__ == '__main__':
     file_name = path.basename(sys.argv[1])
-    length = int(path.basename(sys.argv[2]))
-    width = int(path.basename(sys.argv[3]))
+    base = int(path.basename(sys.argv[2]))
     im = Image.open(file_name)
-    size = (length, width)
+    w, l = im.size
+    size = (int(w/base), int(l/base))
     mode = im.mode
     if mode not in ('L', 'RGB'):
         if mode == 'RGBA':
@@ -20,4 +20,4 @@ if __name__ == '__main__':
         else:
             im = im.convert('RGB')
     thumb = im.resize(size, Image.ANTIALIAS)
-    thumb.save(path+'shrinked_'+file_name.format(i),quality=100)
+    thumb.save('compressed_{}'.format(file_name),quality=100)
