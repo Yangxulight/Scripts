fa12dc005e0b62d2bf9b7d5905354b1f2e71e1af
xuliangyang <yangxulight@outlook.com>
2023-04-20 23:09:18 +0800

    fix width issue

--- shrink_picture.py
+++ shrink_picture.py
@@ -2,11 +2,12 @@ from PIL import Image
 from os import path
 import sys
 
+# usage: python <image> <width> <length>
 # usage: python picture1.jepg 180 240
 if __name__ == '__main__':
-    file_name = path.basename(sys.argv[1])
-    length = int(path.basename(sys.argv[2]))
-    width = int(path.basename(sys.argv[3]))
+    file_name = sys.argv[1]
+    width = int(path.basename(sys.argv[2]))
+    length = int(path.basename(sys.argv[3]))
     im = Image.open(file_name)
     size = (length, width)
     mode = im.mode
@@ -20,4 +21,6 @@ if __name__ == '__main__':
         else:
             im = im.convert('RGB')
     thumb = im.resize(size, Image.ANTIALIAS)
-    thumb.save(path+'shrinked_'+file_name.format(i),quality=100)
+    save_path = path.join(path.dirname(file_name),'shrinked_'+path.basename(file_name))
+    print(f"saving to :{save_path}")
+    thumb.save(save_path,quality=100)
